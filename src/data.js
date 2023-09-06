255b0710825d4f74548acffab5a3e1e35362d770
Monu <yuvraj17802@gmail.com>
2023-09-07 01:19:55 +0530

    third commit

---
+++ src/data.js
@@ -0,0 +1,139 @@
+const data = [
+  {
+    question: "What are the main components of the front end of any working website?",
+    incorrectAnswers: [
+      "HTML only",
+      "Javascript only",
+      "Node.js",
+    ],
+    correctAnswer: "HTML, CSS, Javascript",
+  },
+  {
+    question: "Which property is used to change the background color?",
+    incorrectAnswers: [
+      "color", 
+      "bgcolor", 
+      "bgColor"
+    ],
+    correctAnswer: "background-color",
+  },
+  {
+    question: "How to create an ordered list in HTML?",
+    incorrectAnswers: [
+      "<ul>",
+      "<href>",
+      "<b>",
+    ],
+    correctAnswer: "<ol>",
+  },
+  {
+    question: "Which of the following keywords is used to define a variable in Javascript?",
+    incorrectAnswers: [
+      "var",
+      "let",
+      "None",
+    ],
+    correctAnswer: "var & let both",
+  },
+  {
+    question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+    incorrectAnswers: [
+      "Throws an error", 
+      "Gives a warning", 
+      "Prompts to complete the statement"
+    ],
+    correctAnswer: "Ignores the statements",
+  },
+  {
+    question: "Which of the following methods can be used to display data in some form using Javascript?",
+    incorrectAnswers: [
+      "document.write()", 
+      "console.log", 
+      "window.alert()"
+    ],
+    correctAnswer: "All",
+  },
+  {
+    question: "What keyword is used to check whether a given property is valid or not?",
+    incorrectAnswers: [
+      "is in",
+      "exists",
+      "lies",
+    ],
+    correctAnswer: "in",
+  },
+  {
+    question: "In JavaScript the x===y statement implies that?",
+    incorrectAnswers: [
+      "Both x and y are equal in value, type and reference address as well",
+      "Both are x and y are equal in value only",
+      "Both are not same at all",
+    ],
+    correctAnswer: "Both are equal in the value and data type",
+  },
+  {
+    question: "In the JavaScript, which one of the following is not considered as an error?",
+    incorrectAnswers: [
+      "Syntax error",
+      "Missing of semicolons",
+      "Missing of Bracket",
+    ],
+    correctAnswer: "Division by zero",
+  },
+  {
+    question: "When the switch statement matches the expression with the given labels, how is the comparison done?",
+    incorrectAnswers: [
+      "Only the datatype of the expression is compared",
+      "Only the value of the expression is compared",
+      "None",
+    ],
+    correctAnswer: "Both the datatype and the result of the expression are compared",
+  },
+  {
+    question: "In which language is React.js written?",
+    incorrectAnswers: [
+      "Python",
+      "Java",
+      "PHP",
+    ],
+    correctAnswer: "Javascript",
+  },
+  {
+    question: "What is the default port where webpack-server runs?",
+    incorrectAnswers: [
+      "3000",
+      "3030",
+      "6060",
+    ],
+    correctAnswer: "8080",
+  },
+  {
+    question: "What is ReactJS mainly used for building?",
+    incorrectAnswers: [
+      "Database",
+      "Connectivity",
+      "Design platform",
+    ],
+    correctAnswer: "User interface",
+  },
+  {
+    question: "Why JSX used for?",
+    incorrectAnswers: [
+      "To write jquery in React",
+      "To write SQL in React",
+      "None",
+    ],
+    correctAnswer: "To write HTML in React",
+  },
+  {
+    question: "What is the purpose of JSX in React?",
+    incorrectAnswers: [
+      "To improve the performance of the virtual DOM",
+      "To handle browser events in a more efficient way",
+      "To make it easier to work with state and props",
+    ],
+    correctAnswer: "To provide a way to write HTML elements in JavaScript",
+  },
+];
+
+export default data;
